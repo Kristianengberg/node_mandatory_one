43dc731e051f883f045c0007499c6db949d59acc
Kristian Engberg <krissoengberg@gmail.com>
2021-03-24 16:35:07 +0100

    maybe done?

--- public/scripts/service.js
+++ public/scripts/service.js
@@ -5,3 +5,22 @@ $(function() {
 $(function() {
     $("#footer").load("../components/footer.html");
 });
+
+
+
+function time() {
+    let date = new Date();
+
+    const time =
+        "The time is: " +
+        date.getHours() +
+        ":" +
+        date.getMinutes() +
+        ":" +
+        date.getSeconds();
+
+
+    console.log("time is served");
+    console.log(time);
+    document.getElementById("timeexample").innerHTML = time;
+}
